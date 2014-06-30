55ec99bc9762d2ef8dbec7559976c137dfb6995a
cocal <cocaltzq@gmail.com>
2014-06-30 23:59:12 +0800

    支持多嵌套 但是问题出来了..不支持 2位数 - -

--- rwPlus.py
+++ rwPlus.py
@@ -12,19 +12,30 @@ def testplus(str):
     r1 = re.compile('[0-9]+')
     str1 = []; #操作符队列
     str2 = []; #数值队列
+    currentOpt = '='
+    currentVal = 0
     for x in range(len(str)):
-        if str[x] == '(' :
-            pass
-        elif str[x] == '+' :
-            str1.append(str[x])
-        elif r1.match(str[x]):
-            str2.append(int(str[x]))
+        temp = str[x]
+        if temp == '(' :
+            str2.append(currentVal)
+            currentVal = 0
+        elif temp == '+' :
+            str1.append(temp)
+            currentOpt = temp
+        elif r1.match(temp):
+            if currentOpt == '=' :
+#                 currentVal = currentVal + int(temp)
+                print 'result is ',currentVal 
+            else :
+                currentVal = currentVal + int(temp)
+        elif temp == ')' :
+            currentVal = str2.pop() + currentVal
+            print 'result is %d',currentVal 
             
     print str1
     print str2
 
 
 if __name__ == '__main__':
-    
-    testplus('( + 1 9 )')
+    testplus('( + 1 9 8 (+ 1 2 3) ( + 2 3 ))')
     print 'done'
