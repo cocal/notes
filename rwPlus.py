304372d6803e858b48d50df198f4a6e90cb505c3
cocal <cocaltzq@gmail.com>
2014-07-01 23:53:13 +0800

    设计的满足除法有点麻烦- -

--- rwPlus.py
+++ rwPlus.py
@@ -7,6 +7,7 @@ Created on 2014年6月30日
 
 import re
 
+
 def plus(a=0,b=0):
     return a+b
 
@@ -18,19 +19,19 @@ def mul(a=0,b=0):
 
 def div(a=0,b=1):
     if b == 0:
-        return 'err'
+        return a
     else:
-        return a/b
+        return a/float(b)
 
-opterator = {'+':plus,'-':sub,'*':mul,'/':'mul'}
-
+opterator = {'+':plus,'-':sub,'*':mul,'/': div}
+initOptval = {'+':0,'-':0,'*':1,'/':1}
 def optMath(val1,val2,opt):
     return opterator.get(opt)(val1,val2)
     
 ##前缀表达式
 def testplus(str):
     r1 = re.compile('[0-9]+') #判断是否为数字
-    r2 = re.compile('[\+\*]') #判断是否为属性符号
+    r2 = re.compile('[\+\*/\-]') #判断是否为属性符号
     str1 = []; #操作符队列
     str2 = []; #数值队列
     currentOpt = '='
@@ -38,24 +39,31 @@ def testplus(str):
     for x in range(len(str)):
         temp = str[x]
         if temp == '(' :
+#             print  currentVal
             str2.append(currentVal)
             currentVal = 0
         elif r2.match(temp) :
             str1.append(temp)
             currentOpt = temp
+            currentVal = initOptval.get(temp)
         elif r1.match(temp):
             currentVal = optMath(currentVal,int(temp),currentOpt)
 #             currentVal = currentVal + int(temp)
         elif temp == ')' :
-            currentVal = optMath(currentVal,str2.pop(),currentOpt)
-#             currentVal = str2.pop() + currentVal
             str1.pop()
-            print 'result is %d',currentVal 
+            if len(str1) == 0:
+                print 'result is %d',currentVal 
+            else :
+                currentVal = optMath(currentVal,str2.pop(),str1[-1])
+#             currentVal = str2.pop() + currentVal      
             
-    print str1
-    print str2
+            
+#     print str1
+#     print str2
 
 
 if __name__ == '__main__':
-    testplus('( + 1 9 8 (* 1 2 3) ( + 2 3 ))')
+    testplus('(/ 4 2)')
+#     testplus('( + 1 9 8 (* 1 2 3) ( / 2 3 ))')
+#     print 4/float(8)
     print 'done'
