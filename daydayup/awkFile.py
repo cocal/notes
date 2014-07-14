a580c72198871698c7465f958a4cd52f229fa671
cocalrush <cocaltzq@gmail.com>
2014-07-14 17:18:36 +0800

    简单实现文本解析功能 类似awk

---
+++ daydayup/awkFile.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+## 实现一个批处理行
+
+f = open('1')
+
+dict = {}
+#line = f.readline()
+#while line != None :
+#    token = line.split(' ')
+#    len
+#    print token
+
+
+str = r"呵呵 1 呵呵 2"
+token = str.split(' ')
+print token[11]
