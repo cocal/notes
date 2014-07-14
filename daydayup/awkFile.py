3e9a6637beb83595406289ee1f1aa194990ca49f
cocal <cocaltzq@gmail.com>
2014-07-14 23:50:35 +0800

    技能get

--- daydayup/awkFile.py
+++ daydayup/awkFile.py
@@ -1,17 +1,36 @@
 # -*- coding: utf-8 -*-
 
 ## 实现一个批处理行
-
+## 基本上嫩实现一个计数功能了
 f = open('1')
 
-dict = {}
-#line = f.readline()
-#while line != None :
-#    token = line.split(' ')
-#    len
-#    print token
+di = {}
+line = f.readline()
+while line != "" :
+    token = line.split(' ')
+    if len(token) <= 11:
+        print line
+        continue
+    ip = token[11]
+    print ip 
+    if di.has_key(ip) :
+        nu = di.get(ip) + 1
+        di[ip] = nu
+    else :
+        di[ip] = 1
+    line = f.readline()
+f.close()
+print di
 
 
-str = r"呵呵 1 呵呵 2"
-token = str.split(' ')
-print token[11]
+# str = r"'2014-07-12 00:00:00,799', 'INFO', 'WebContainer : 5', 'webservice.interceptor.IpInterceptor', 'IP拦截器开始  IP : 101.101.101.101 请求接口 :HELO'"
+# token = str.split(' ')
+# print token[11]
+# ip = token[11]
+# if di.has_key(ip) :
+#     nu = di.get(ip) + 1
+#     di[ip] = nu
+# else :
+#     di[ip] = 1
+# print di
+
