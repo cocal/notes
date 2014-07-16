57301bcc2c51d09ba34c3f56728491a709ffea51
cocal <cocaltzq@gmail.com>
2014-07-16 23:27:24 +0800

    Update palindrome.py

--- sp/palindrome.py
+++ sp/palindrome.py
@@ -29,3 +29,25 @@ def isPalindrome2(str):
 
 print isPalindrome2(str)
 
+##找出字符串中最长回文
+
+def LongestPalindrome(str):
+    l = len(str)
+    max = 0
+    for i in range(l) :
+        k = 0
+        while (i-k > 0) and (i + k < l) : #只考虑奇数情况
+            if str [i-k] != str[i+k] :
+#                 print i,str[i-k],str[i+k] 
+                k = k -1
+                break
+            else :
+                k = k + 1
+
+        if max < k * 2 + 1:
+            max = k * 2 + 1
+            print i
+            print str[i-k:i+k+1]
+    print max
+
+LongestPalindrome('sdscabdbaccc')
