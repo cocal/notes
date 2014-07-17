991cf5f698ffe35e6587de507a29cd7e05d5122a
cocal <cocaltzq@gmail.com>
2014-07-17 23:20:40 +0800

    增加偶数方法

--- sp/palindrome.py
+++ sp/palindrome.py
@@ -29,43 +29,37 @@ def isPalindrome2(str):
 
 print isPalindrome2(str)
 
-
 ##找出字符串中最长回文
 
 def LongestPalindrome(str):
     l = len(str)
-    print l
     max = 0
     for i in range(l) :
         k = 0
-        while (i-k > 0) and (i + k < l) : #只考虑奇数情况 ##有bug..
+        while (i-k > 0) and (i + k < l) : #只考虑奇数情况
             if str [i-k] != str[i+k] :
-                print i,str[i-k],str[i+k] 
-                k = k -1
+#                 print i,str[i-k],str[i+k] 
+                k = k - 1
                 break
             else :
                 k = k + 1
-                
+
         if max < k * 2 + 1:
             max = k * 2 + 1
             print i
             print str[i-k:i+k+1]
-         
-        while (i-k > 0) and (i + k - 1 < l) : #只考虑偶数数情况
-            if str [i-k] != str[i+k-1] :
-#                 print i,str[i-k],str[i+k] 
-                k = k -1
+            
+        j = 0
+        while (i - j > 0) and (i + k < l) :
+            if str [i-j] != str [i+j-1] :
+                j = j - 1
                 break
             else :
-                k = k + 1   
-        if max < k * 2 :
-            max = k * 2
+                j = j + 1
+        if max < j *2 :
+            max = j * 2
             print i
-            print str[i-k:i+k]
+            print str[i-j:i+j+1]
     print max
 
 LongestPalindrome('sdscabbaccc')
-        
-    
-    
-    
