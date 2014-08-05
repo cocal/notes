1821211ce6e88c57cd463f488c5d2dc6998eaff0
cocal <cocaltzq@gmail.com>
2014-08-05 22:32:21 +0800

    快排变种2

--- daydayup/footstep.py
+++ daydayup/footstep.py
@@ -13,5 +13,37 @@ def getSteps(n):
         res = currentSteps + lastSteps
         lastSteps, currentSteps = currentSteps, res
     return res
-for i in range(10):
-    print getSteps(i)
+# for i in range(10):
+#     print getSteps(i)
+    
+    
+##荷兰三色国旗问题 
+##现有一串由1，2，3组成的乱序数列 [1,2,3,3,2,1,1,2,3,3,2,2] 要求按[1,1,1,2,2,2,3,3]格式排列
+
+##也是一个快排的变种
+li = [1,2,3,3,2,1,1,2,3,3,2,2]
+def threeBolls(li):
+    print li
+    lenth = len(li) - 1
+    begin = 0
+    current = 0
+    end = lenth
+    while current < end :
+#         print li,current
+        if li[current] == 1 : 
+            li[begin], li[current] = li[current], li[begin]
+            begin += 1
+            current += 1
+        elif li[current] == 2 :
+            current +=1
+            continue
+        elif li[current] == 3 :
+            li[current],li[end] = li[end],li[current]
+            end -= 1
+    print li 
+    
+threeBolls(li);
+
+
+
+
