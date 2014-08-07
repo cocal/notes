1ffd5e7b6a9ace9bfaa25f36ae8b1531235fc1dd
cocal <cocaltzq@gmail.com>
2014-08-07 23:29:39 +0800

    先这样吧 - -

--- daydayup/binarytree.py
+++ daydayup/binarytree.py
@@ -12,31 +12,36 @@ class binarryTree():
     def __init__(self):
         self.root = Node()
    
-    def insertNode(self,date):
+    def insertNode(self,date): 
+        print date     
         node = Node(val=date)
-        
         r = self.root
         while r != None :
             p = r
-            if r.left > node.val : 
+            if r.left == None and r.right == None : 
+                r = r.left
+            elif r.left != None and r.right == None :
+                r = r.right
+            elif r.left.left == None:
                 r = r.left
             else :
                 r = r.right
-        if p.val > node.val :
+        if p.left == None :
             p.left = node
         else :
             p.right = node
     
-def showTheTree(node):
+def showTheTree(node,deep = 0):
     if node == None :
         return
-    print node.val
-    showTheTree(node.left)
-    showTheTree(node.right)
+    print node.val,deep
+    deep += 1
+    showTheTree(node.left, deep)
+    showTheTree(node.right,deep)
 
 
 b = binarryTree()
 
-for i in [1,2,3,4,7,8,6,4] :
+for i in [1,2,3,4,5,7,8,6,4] :
     b.insertNode(i)   
 showTheTree(b.root)     
